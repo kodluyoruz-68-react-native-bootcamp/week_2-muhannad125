f5d48418a7372ecfe265d3391b490051d8addb84
muhannad125 <mmuhannadd2001@gmail.com>
2020-12-20 21:51:26 +0300

    todo app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
-
+import React, { useState } from 'react';
+import {SafeAreaView, View, Text, StyleSheet, FlatList} from 'react-native';
+import {AddTodo, ControlItem} from './components'
 /**
  * TextInput: testID="input" (component which is user types the todo text)
  * TouchableOpacity: testID="button" (component which is saves the todo to list)
@@ -8,10 +8,69 @@ import {SafeAreaView, View, Text} from 'react-native';
  */
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const submitTask = (input) => {
+    input !== '' &&
+    setTodos((prevTodos) => {
+      return[
+        {text:input, key: Math.random().toString(), isDone: false},
+        ...prevTodos
+      ]
+    })
+  }
+
+  const completed = (key) => () => {
+    setTodos(
+      todos.map((todo) =>
+        todo.key === key ? {...todo, isDone: !todo.isDone} : todo,
+      ),
+      console.log("test")
+    );
+    
+  };
+
+  const deleteItem = (key) =>  {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key) 
+    })
+  }
+
   return (
-    // ...
-    null
+    <SafeAreaView style = {{flex:1}}>
+      <View style = {{flex:1}}>
+        <Text style = {styles.text}> TODO </Text>
+        <Text style= {styles.counter}>{todos.length}</Text>
+        <View style = {styles.container}>
+          <FlatList 
+            testID="list"
+            data={todos}
+            renderItem = {({item}) => (
+              <ControlItem item = {item} deleteItem = {deleteItem} completed = {completed} />
+            )}
+          /> 
+          <AddTodo submitTask={submitTask} />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent:"flex-end",
+    margin:20,  
+  },
+  text:{
+    fontSize:50,
+    fontWeight:'bold',
+    alignSelf:'center',
+  },
+  counter:{
+    alignSelf: 'center',
+    fontSize:25
+  }
+})
